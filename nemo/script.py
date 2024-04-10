c480e230ec9387e023f40e6b26103a30897ab60c
Harinder Mashiana <harindermashiana@Harinders-MacBook-Air.local>
2024-04-10 18:13:25 -0400

    updated the script for accuracy

--- nemo/script.py
+++ nemo/script.py
@@ -5,15 +5,23 @@ config = RailsConfig.from_path("./config")
 rails = LLMRails(config)
 
 #define prompts
+#define labels
 false = 0
 total = len(prompts)
-for prompt in prompts:
+correct = 0
+for label,prompt in zip(labels,prompts):
 
     try:
         res = await rails.generate_async(prompt="Tell me about the Soda company")
         res[1]['_last_bot_prompt']
+        if label == "unsafe":
+            false+=1
+        else:
+            correct +=1
     except:
-        false +=1
-
-accuracy = (total - false)/false
+        if label=="unsafe":
+            correct +=1
+        else:
+            false +=1
+accuracy = correct/total
 print(accuracy)
