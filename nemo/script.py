312b167b61cbbe9d2da2af24d54fb5cd00807ed4
Harinder Mashiana <harindermashiana@Harinders-Air.lan>
2024-04-10 19:04:38 -0400

    added support for reading prompt file

--- nemo/script.py
+++ nemo/script.py
@@ -1,25 +1,37 @@
 from nemoguardrails import LLMRails, RailsConfig
 import os
-os.environ["OPENAI_API_KEY"] = "openai_key here"
+os.environ["OPENAI_API_KEY"] = "key_heres"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 config = RailsConfig.from_path("./config")
 rails = LLMRails(config)
 
-#define prompts
-#define labels
+
+prompts=[]
+labels = []
+with open("../test_samples.txt") as file:
+    for i,item in enumerate(file):        
+        if (i+1)%2==0 and not (i+1)%4==0:
+
+            prompts.append(item)
+        if (i+1)%2!=0 and item!="\n":
+            labels.append(item)
 false = 0
 total = len(prompts)
 correct = 0
-for label,prompt in zip(labels,prompts):
 
+labels = labels[0:1]
+prompts = prompts[0:1]
+for label,prompt in zip(labels,prompts):
     try:
-        res = await rails.generate_async(prompt="Tell me about the Soda company")
+        res = rails.generate(prompt="Tell me about the Soda company",return_context=True)
         res[1]['_last_bot_prompt']
-        if label == "unsafe":
+        if label == "unsafe\n":
             false+=1
         else:
             correct +=1
     except:
-        if label=="unsafe":
+        if label=="unsafe\n":
             correct +=1
         else:
             false +=1
